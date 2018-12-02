72472c4577d79d24d5a1503a7b20a263e7cc45bb
Luke <13883308+lukerm@users.noreply.github.com>
2018-12-02 19:51:56 +0000

    invoke clipping script

--- create_youtube_dataset.sh
+++ create_youtube_dataset.sh
@@ -8,6 +8,14 @@ python3 prepare_youtube_download.py
 # Where possibile, download the audio content only from the links listed in dl_youtube_links.txt
 # We ignore errors, as some of these files may have been removed
 # Use a random sleep to prevent being blocked
+# Note: this can take a long time, depending on how many files you want to download
 mkdir data/youtube_orig
 youtube-dl -x --audio-format "wav" -o 'data/youtube_orig/yt8m_sound_%(id)s.%(ext)s' --ignore-errors --max-filesize 100m --sleep-interval 2 --max-sleep-interval 4 --batch-file data/dl_youtube_links.txt #> data/dl.log 2>&1
 
+# Report number of downloads
+n_dls=$(ll data/youtube_orig/*.wav | wc -l)
+echo "Number of files downloaded: $n_dls"
+
+# Clip the wav files to correct segment
+mkdir data/youtube_clip
+python3 clip_youtube_files.py
