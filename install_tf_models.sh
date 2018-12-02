015ac5c3f314d2b41024756abf294728aee32c6c
Luke <13883308+lukerm@users.noreply.github.com>
2018-12-02 11:20:27 +0000

    installation procedure: see https://github.com/tensorflow/models/tree/master/research/audioset

---
+++ install_tf_models.sh
@@ -0,0 +1,14 @@
+# Requirements
+sudo python3 -m pip install numpy scipy
+sudo python3 -m pip install resampy tensorflow six
+
+# Clone tensorflow's repo (approx 0.5GB)
+git clone git@github.com:tensorflow/models.git ~/tf-models
+cd ~/tf-models/research/audioset
+
+# Download models / parameters
+wget https://storage.googleapis.com/audioset/vggish_model.ckpt # 0.25GB
+wget https://storage.googleapis.com/audioset/vggish_pca_params.npz # 70KB
+
+# Perform installation test
+python3 vggish_smoke_test.py
