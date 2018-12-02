2b2c278d0dec984d1b693c5e49db53fe05e6d9a5
Luke <13883308+lukerm@users.noreply.github.com>
2018-12-02 21:10:21 +0000

    loading youtube sound bite data, convert to format recognized by tf model

--- get_target_data.py
+++ get_target_data.py
@@ -1,3 +1,5 @@
+## Binary classification: target track (1) or other music / noise (0)
+
 
 ## Imports ##
 
@@ -20,16 +22,36 @@ VGGISH_DIR= os.path.join(user_root, 'tf-models','research','audioset')
 checkpoint_path = os.path.join(VGGISH_DIR, 'vggish_model.ckpt')
 pca_params_path = os.path.join(VGGISH_DIR, 'vggish_pca_params.npz')
 
+# Paths to data files
 DATA_DIR = os.path.join(user_root, 'find-tune', 'data')
+YT8M_DIR = os.path.join(DATA_DIR, 'youtube_clip')
 
 
 ## Main ## 
 
-
 # Get embedding features for the target file
 tgt_data_lmel = vggish_input.wavfile_to_examples(os.path.join(DATA_DIR, 'target_tune.wav'))
 
+# Details for all the links
+with open(os.path.join(DATA_DIR, 'link_details.json'), 'r') as j:
+    link_details = json.load(j)
 
+# Keep a record of the categories per sound bite
+ytid_data = {}
+for cat, urls in link_details.items():
+    print('category: %s' % cat)
+    for _, yt_id, _, _ in urls:
+        # Skip this link if not in the YT8M_DIR
+        wav_fpath = os.path.join(YT8M_DIR, 'yt8m_sound_%s.wav' % yt_id)
+        if not os.path.isfile(wav_fpath):
+            print(yt_id)
+            continue
+        # Record the category and log-mel spectrogram for this file
+        ytid_data[yt_id] = {}
+        ytid_data[yt_id]['category'] = cat
+        ytid_data[yt_id]['log_mel']  = vggish_input.wavfile_to_examples(wav_fpath)
+
+#
 with tf.Graph().as_default(), tf.Session() as sess:
   vggish_slim.define_vggish_slim()
   vggish_slim.load_vggish_slim_checkpoint(sess, checkpoint_path)
