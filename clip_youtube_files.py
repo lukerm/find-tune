8341f4396f9d6a9a1535a77a78358955d9fb093b
Luke <13883308+lukerm@users.noreply.github.com>
2018-12-02 19:51:01 +0000

    clip downloaded YouTube files to YouTube8m length

---
+++ clip_youtube_files.py
@@ -0,0 +1,27 @@
+## Imports ##
+
+import os
+import json
+
+
+## Main ##
+
+with open('data/link_details.json', 'r') as j:
+    link_details = json.load(j)
+
+n_success, n_failure = 0, 0
+for cat, urls in link_details.items():
+    for url, yt_id, start, end in urls:
+        orig_fname = os.path.join('data', 'youtube_orig', 'yt8m_sound_%s.wav' % yt_id)
+        clip_fname = os.path.join('data', 'youtube_clip', 'yt8m_sound_%s.wav' % yt_id)
+        status = os.system('ffmpeg -loglevel quiet -ss %d -i %s -to %d %s' % (int(start), orig_fname, int(end), clip_fname))
+        
+        if status != 0: 
+            n_failure += 1
+            print("FAILURE: %s (doesn't exist)" % yt_id)
+        else:
+            n_success += 1
+            print("SUCCESS: %s" % yt_id)
+
+print('Number of files clipped: %d' % n_success)
+print('Number of failed files:  %d' % n_failure)
