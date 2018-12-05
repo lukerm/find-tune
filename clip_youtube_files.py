72f9988c2f23c82f1de39890d02353e7afdb3994
Luke <13883308+lukerm@users.noreply.github.com>
2018-12-05 22:31:44 +0000

    bug fix: ffmpeg -to requires duration, not endpoint

--- clip_youtube_files.py
+++ clip_youtube_files.py
@@ -12,11 +12,16 @@ with open('data/link_details.json', 'r') as j:
 n_success, n_failure = 0, 0
 for cat, urls in link_details.items():
     for url, yt_id, start, end in urls:
+
+        # Required for the -to arg in ffmpeg
+        duration = int(end) - int(start)
+        assert duration > 0 # Usually 10s
+
         orig_fname = os.path.join('data', 'youtube_orig', 'yt8m_sound_%s.wav' % yt_id)
         clip_fname = os.path.join('data', 'youtube_clip', 'yt8m_sound_%s.wav' % yt_id)
-        status = os.system('ffmpeg -loglevel quiet -ss %d -i %s -to %d %s' % (int(start), orig_fname, int(end), clip_fname))
-        
-        if status != 0: 
+        status = os.system('ffmpeg -loglevel quiet -ss %d -i %s -to %d -c copy %s' % (int(start), orig_fname, duration, clip_fname))
+
+        if status != 0:
             n_failure += 1
             print("FAILURE: %s (doesn't exist)" % yt_id)
         else:
