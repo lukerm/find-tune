c74bff252f053f879461adc252b26d7768ce0f0c
lukerm <13883308+lukerm@users.noreply.github.com>
2018-12-14 07:40:19 +0000

    unfinished version of a light loading Initializer

---
+++ prod/initialize.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+from keras.initializers import Initializer
+
+class LightLoadInitializer(Initializer):
+    def __init__(self, layer_name, bias, weights_dir):
+        self.layer_name = layer_name
+        self.bias = bias
+        self.weights_dir= weights_dir
+
+    def __call__(self, shape=None, dtype=None):
+        wts = np.load(os.path.join(self.weights_dir), '%s.npz' % layer_name)
+        w, b= wts['w'], wts['b']
+        # TODO: return Tensor, with the same values
+        if bias:
+            return b
+        else:
+            return w
+
+    def get_config(self):
+        return {
+            'layer_name': self.layer_name,
+            'bias': self.bias,
+            'weights_dir': self.weights_dir,
+        }
