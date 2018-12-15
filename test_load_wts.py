534e0fcc65cbfacc99749903e3107a82f3ef89ff
lukerm <13883308+lukerm@users.noreply.github.com>
2018-12-15 08:07:57 +0000

    load using LightLoadInitializer

--- test_load_wts.py
+++ test_load_wts.py
@@ -2,12 +2,17 @@ import os
 import json
 import numpy as np
 from keras.models import model_from_json
+from prod.initialize import LightLoadInitializer
 
 DATA_DIR = './data'
 
-with open(os.path.join(DATA_DIR, 'my_vggish_network.json'), 'r') as j:
+with open(os.path.join(DATA_DIR, 'my_vggish_network_lite.json'), 'r') as j:
     model_dict = json.load(j)
-vggish = model_from_json(json.dumps(model_dict))
+
+vggish = model_from_json(
+            json_string = json.dumps(model_dict),
+            custom_objects = {'LightLoadInitializer': LightLoadInitializer}
+         )
 print('architecture done', flush=True)
 
 # Where each layer's weights are stored on disk
