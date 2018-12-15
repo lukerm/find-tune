af5bb635a5fe196333dce43cc137245222495f91
lukerm <13883308+lukerm@users.noreply.github.com>
2018-12-15 19:33:08 +0000

    remove redundant code from test file

--- test_load_wts.py
+++ test_load_wts.py
@@ -4,6 +4,7 @@ import numpy as np
 from keras.models import model_from_json
 from prod.initialize import LightLoadInitializer
 
+# TODO: fix path!
 DATA_DIR = './data'
 
 with open(os.path.join(DATA_DIR, 'my_vggish_network_lite.json'), 'r') as j:
@@ -13,20 +14,4 @@ vggish = model_from_json(
             json_string = json.dumps(model_dict),
             custom_objects = {'LightLoadInitializer': LightLoadInitializer}
          )
-print('architecture done', flush=True)
-
-# Where each layer's weights are stored on disk
-with open(os.path.join(DATA_DIR, 'layers', 'layer_loc.json'), 'r') as j:
-    layer_loc = json.load(j)
-#for lyr, fp in layer_loc.items():
-for lyr in ['fc1_1']:
-    print('\t doing %s' % lyr, flush=True)
-    fp = 'layers/%s.npz' % lyr
-    wts  = np.load(os.path.join(DATA_DIR, fp))
-    print('load done', flush=True)
-    w, b = wts['w'], wts['b']
-    print('now setting weights', flush=True)
-    vggish.get_layer(lyr).set_weights([w,b])
-    del w, b, wts
-    print('\t%s done' % lyr, flush=True)
-print('weights done', flush=True)
+print('done', flush=True)
