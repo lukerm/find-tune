25c63fff7f46ff7f2b5e6cb8b6bf4024de24fd3b
lukerm <13883308+lukerm@users.noreply.github.com>
2019-02-16 12:49:18 +0000

    script to test if model loads into memory

---
+++ prod/test_load_model.py
@@ -0,0 +1,12 @@
+import os
+import json
+from keras.models import load_model, model_from_json
+
+DATA_DIR = '../data'
+
+with open(os.path.join(DATA_DIR, 'my_vggish_network.json'), 'r') as j:
+    model_dict = json.load(j)
+vggish = model_from_json(json.dumps(model_dict))
+vggish.load_weights(os.path.join(DATA_DIR, 'my_vggish_network.h5'))
+
+print('loaded')
