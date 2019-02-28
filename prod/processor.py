429353551ccab875d013a7dfb96ff34630357ccd
lukerm <13883308+lukerm@users.noreply.github.com>
2019-02-28 21:01:44 +0000

    apply proper attribution to modified scripts

--- prod/processor.py
+++ prod/processor.py
@@ -1,12 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Dec  9 15:18:47 2018
+# Copyright (C) 2017 DataArt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This file has been (extensively) modified by lukerm (2018). View the original file at:
+#     https://github.com/devicehive/devicehive-audio-analysis/blob/master/audio/processor.py
 
-@author: luke
-"""
 
-## This script processes incoming sound data and classifies it using our tuned VGGish network. It is (heavily) adapted  ##
-## from: https://github.com/devicehive/devicehive-audio-analysis/blob/master/audio/processor.py                         ##
+## This script processes incoming sound data and classifies it using our tuned VGGish network ##
 
 
 ## Imports ##
