e850201c037036dd215a6e9ef0250ef932d42bb3
lukerm <13883308+lukerm@users.noreply.github.com>
2019-02-28 21:36:05 +0000

    add Apache License 2.0 to scripts

--- prod/log_config.py
+++ prod/log_config.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2017 DataArt
+# Modifications copyright (C) 2018 lukerm
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
