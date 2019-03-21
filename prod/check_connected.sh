c635fe1de7d81c8abf4ee346d287f09cdfa87567
lukerm <13883308+lukerm@users.noreply.github.com>
2019-03-21 20:07:14 +0000

    add Apache license

--- prod/check_connected.sh
+++ prod/check_connected.sh
@@ -1,3 +1,18 @@
+#  Copyright (C) 2019 lukerm
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# Complete with MAC address of the bluetooth device you will connect
 TARGET_MAC=**:**:**:**:**:**
 
 BT_DEV_MAC=$(pactl list cards short | grep -i 'bluez_card' | sed -E 's/.*bluez_card\.(([0-9A-F]_?){6})/\1/' | cut -c 1-17 | sed 's/_/:/g')
