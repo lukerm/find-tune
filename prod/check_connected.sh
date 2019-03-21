38861158fec5a6691f1fa56cc7abe3d747dac0cc
lukerm <13883308+lukerm@users.noreply.github.com>
2019-03-21 20:02:33 +0000

    bug fix: misspelt target bluetooth profile

--- prod/check_connected.sh
+++ prod/check_connected.sh
@@ -3,7 +3,7 @@ TARGET_MAC=**:**:**:**:**:**
 BT_DEV_MAC=$(pactl list cards short | grep -i 'bluez_card' | sed -E 's/.*bluez_card\.(([0-9A-F]_?){6})/\1/' | cut -c 1-17 | sed 's/_/:/g')
 PROFILE=$(pactl list sinks | grep -i 'bluetooth.protocol' | sed -E 's/.*"([a-z0-9_]+)"/\1/')
 
-if [[ $BT_DEV_MAC == $TARGET_MAC ]] && [[ $PROFILE == "a2dp_sink_woops" ]]
+if [[ $BT_DEV_MAC == $TARGET_MAC ]] && [[ $PROFILE == "a2dp_sink" ]]
 then
   echo "connected"
 else
